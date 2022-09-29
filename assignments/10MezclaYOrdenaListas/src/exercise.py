e19e1cf1aa899ef57da611e21f425fdc3042f4ae
a01285209 <111028614+a01285209@users.noreply.github.com>
2022-09-29 08:30:52 -0500

    Update exercise.py

--- assignments/10MezclaYOrdenaListas/src/exercise.py
+++ assignments/10MezclaYOrdenaListas/src/exercise.py
@@ -1,6 +1,33 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    L1= []
+    while True:
+        numero=(input)
+        
+        if numero == '*':
+            break
+        L1.append(int(numero))
+    
+    L2=[]
+    while True:
+        numero=(input)
+        
+        if numero=='*':
+            break
+        L2.append(int(numero))
+    
+    print('L1=')
+    print(L1)
+
+    print('L2=')
+    print(L2)
+
+    L3=[]
+    L3= L1+L2
+    L3.sort()
+
+    print('LORDENADA=')
+    print(L3)
 
 if __name__=='__main__':
     main()
